a7c1bc2ad72f150eeaa5fb0bcee9e3c365bc913c
Willian Viana <willian.batista.viana@gmail.com>
2017-11-21 18:11:58 -0200

    adjusted eslint and removed app component

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
@@ -72,8 +72,8 @@ module.exports = function (proxy, allowedHost) {
         `^(?!${path
           .normalize(`${paths.appSrc}/`)
           .replace(/[\\]+/g, '\\\\')}).+[\\\\/]node_modules[\\\\/]`,
-        'g',
-      ),
+        'g'
+      )
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends Component {
+  render() {
+    return <p> Hello World </p>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
